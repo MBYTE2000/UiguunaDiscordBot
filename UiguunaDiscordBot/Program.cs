03c585702b77d8dd733ff4e3a3d2b48d157fe295
megob <megob@MBYTE2000>
2023-02-22 15:47:02 +0300

    Добавьте файлы проекта.

---
+++ UiguunaDiscordBot/Program.cs
@@ -0,0 +1,55 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.Addons.Hosting;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace UiguunaDiscordBot
+{
+    internal class Program
+    {
+        static async Task Main()
+        {
+            var builder = new HostBuilder()
+                .ConfigureAppConfiguration(x =>
+                {
+                    var configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("config.json", false, true)
+                    .Build();
+
+                    x.AddConfiguration(configuration);
+                })
+                .ConfigureLogging(x =>
+                {
+                    x.AddConsole();
+                    x.SetMinimumLevel(LogLevel.Debug);
+                })
+                .ConfigureDiscordHost((context, config) =>
+                {
+                    config.SocketConfig = new DiscordSocketConfig
+                    {
+                        LogLevel = LogSeverity.Debug,
+                        AlwaysDownloadUsers = true,
+                        MessageCacheSize = 200,
+                    };
+
+                    config.Token = context.Configuration["Token"];
+                })
+                .UseCommandService((context, config) =>
+                {
+                    config.CaseSensitiveCommands = false;
+                    config.LogLevel = LogSeverity.Debug;
+                    config.DefaultRunMode = RunMode.Sync;
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    //services.AddHostedService<CommandHandler>();
+                })
+                .UseConsoleLifetime();
+        }
+    }
+}
